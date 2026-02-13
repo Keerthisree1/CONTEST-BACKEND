a06291d7d1cb0b9f8d8660c6a4cc02aa46310105
Keerthisree1 <miriyalakeerthi49@gmail.com>
2026-02-13 09:18:24 +0530

    controller

--- controllers/contestController.js
+++ controllers/contestController.js
@@ -32,3 +32,58 @@ exports.getContests = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+//filter
+
+const mongoose = require("mongoose");
+
+exports.getContests = async (req, res) => {
+  try {
+    const { contestId, status, title, experience, budget } = req.query;
+
+    let filter = {};
+
+    if (contestId) {
+      filter.contestId = new mongoose.Types.ObjectId(contestId);
+    }
+
+    if (status) {
+      filter.contestStatus = status;
+    }
+
+    if (title) {
+      filter["details.jobDetails.jobTitle"] = {
+        $regex: title,
+        $options: "i"
+      };
+    }
+
+    if (experience) {
+      filter["details.jobDetails.experience"] = {
+        $regex: experience,
+        $options: "i"
+      };
+    }
+
+    if (budget) {
+      filter["details.jobDetails.budget"] = {
+        $regex: budget,
+        $options: "i"
+      };
+    }
+
+    const contests = await Contest.find(filter);
+
+    res.json({
+      success: true,
+      total: contests.length,
+      data: contests
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
